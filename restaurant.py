6520167525fe393bc1cb1419349da49e7adeabb5
PatrickBett <patrickbett018@gmail.com>
2023-12-11 13:13:09 +0300

    initialized the class

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,34 @@
-class Restaurant:
+#imports 
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import declarative_base
+
+
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    def _init_(self,name,price):
+        self.id = None
+        self.name = name
+        self.price = price
+    
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    def _init_(self,name,price):
+        self.id = None
+        self.name = name
+        self.price = price
     pass
-class Review:
-    pass
-class Customer:
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    def _init_(self,firstname,lastname):
+        self.id = None
+        self.firstname = firstname
+        self.lastname = lastname
     pass
