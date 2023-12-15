104033134ae5b17a5f30369eaa87c48bd8cb4fa5
PatrickBett <patrickbett018@gmail.com>
2023-12-15 21:00:46 +0300

    done

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-#imports 
-from sqlalchemy import Column, String, Integer
+#imports
+from sqlalchemy.orm import relationship 
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 
@@ -12,6 +13,9 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column (String(100))
     price = Column (Integer)
+
+    customers = relationship('Customer', secondary='reviews')
+    reviews = relationship('Review', back_populates='restaurant')
  
 class Review(Base):
     __tablename__ = 'reviews'
@@ -20,11 +24,20 @@ class Review(Base):
     customer_fullname = Column(String(50))
     star_rating = Column (Integer)
 
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    
+    customer = relationship('Customer', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
+
 class Customer(Base):
     __tablename__ = 'customers'
     id = Column(Integer, primary_key=True, autoincrement=True)
     firstname = Column (String(50))
     lastname = Column (String(50))
+
+    reviews = relationship('Review', back_populates='customer')
+    
 
 
        
